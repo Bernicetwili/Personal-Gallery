7068f87da4605077439fa9d4327dd11561a3edc8
Bernice Twili <bernicetwili0@gmail.com>
2021-05-20 14:23:00 +0300

    write the category test

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from .models import Category,Photo
 # Create your tests here.
+
+class CategoryTest(TestCase):
+    def setUp(self):
+        self.category = Category(name='food')
+        self.category.save_category()
+        
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_category(self):
+        self.category.save_category()
+        cat = Category.objects.all()
+        self.assertTrue(len(cat) > 0)
