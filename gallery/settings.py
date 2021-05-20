e2848858472b14a54fd8ed53ca7f33aff2448722
Bernice Twili <bernicetwili0@gmail.com>
2021-05-20 08:17:23 +0300

    add admin

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    #'photos',
     'photos.apps.PhotosConfig',
 ]
 
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -122,7 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [BASE_DIR / 'static']
+# configuring the location for media
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- photos/models.py
+++ photos/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Category()
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+    
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+        
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
