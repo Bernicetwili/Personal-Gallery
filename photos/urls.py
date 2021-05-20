b608cb9c211b4673f39fb25639f7cb3e3ca32482
Bernice Twili <bernicetwili0@gmail.com>
2021-05-20 08:25:38 +0300

    adding models to the admin page

--- photos/urls.py
+++ photos/urls.py
@@ -4,8 +4,15 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = 'photos'
+
 urlpatterns = [
     path('', views.gallery, name='gallery'),
     path('photo/<str:pk>/', views.viewPhoto, name='photo'),
     path('add/', views.addPhoto, name='add'),
-]
+]+ static(
+  settings.MEDIA_URL, 
+  document_root = settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
